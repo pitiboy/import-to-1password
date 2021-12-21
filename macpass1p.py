966a5bc535d0d48c2b0be93a643d435fa112f1ab
pitiboy <peter.horvath@finnoconsult.at>
2021-12-21 15:53:05 +0100

    add filtering support based on folders, filtering by xml input groups

--- macpass1p.py
+++ macpass1p.py
@@ -2,6 +2,8 @@
 
 import os
 import logging
+import sys
+import re
 
 import ConfigParser
 import argparse
@@ -31,34 +33,47 @@ config.read(os.path.join('etc', 'settings.conf'))
 passwords_xml = BeautifulSoup(open(config.get('General', 'input')), 'lxml')
 logger.info('MacPass XML file is opened')
 
-passwords = []
+secrets = []
 
 for entry in passwords_xml.find_all('entry'):
   if entry.find_parent('history'):
     continue
 
+  matches = False
+  folders = []
+  parentGroups = entry.find_parents('group')
+  for group in parentGroups:
+    if not re.match(config.get('xml', 'regexExcludeFolder'), group.find('name').string):
+      folders.append(group.find('name').string.lower())
+    if group.find('name').string == config.get('xml', 'root'):
+      matches = True
 
-  password = {}
-  # tag fields
+  if matches == False:
+    continue
+
+  secret = {}
+  # collect fields
   fields = {}
 
   for tag in entry.find_all('string'):
     if not tag.find_parent('history'):
       fields[tag.key.string.lower()] = tag.value.string
 
-  password['title'] = normalize(fields['title'])
-  password['username'] = normalize(fields['username'])
-  password['password'] = normalize(fields['password'])
-  password['url'] = normalize(fields['url'].replace('http://', '')) if fields['url'] else ''
-  password['notes'] = normalize(fields['notes'])
+  secret['title'] = normalize(fields['title'])
+  secret['username'] = normalize(fields['username'])
+  secret['password'] = normalize(fields['password'])
+  secret['url'] = normalize(fields['url'].replace('http://', '')) if fields['url'] else ''
+  secret['notes'] = normalize(fields['notes'])
+  secret['tags'] = normalize(",".join(folders))
 
-  passwords.append(password)
+  secrets.append(secret)
+
 
 # Prepare output file
 env = Environment(loader=PackageLoader('__main__', 'templates'))
 template = env.get_template('passwords.tmpl')
-output = open(config.get('General', 'output'), 'w')
-output.write(template.render(passwords = passwords).encode('utf-8'))
+output = open(config.get('General', 'output')+'-'+config.get('xml', 'root')+config.get('General', 'outputExtension'), 'w')
+output.write(template.render(passwords = secrets).encode('utf-8'))
 output.close()
 
 logger.info('1Password CSV file is written')
